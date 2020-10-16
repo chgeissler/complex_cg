90ab3a806d331d2ec54f9cc7e37e093e620a56f4
chgeissler <cgeissler@advestis.com>
2020-10-16 14:08:38 +0200

    New: second commit

--- project_template/complex.py
+++ project_template/complex.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 
 class ComplexForDummies:
@@ -89,30 +90,38 @@ class ComplexForDummies:
     def __str__(self):
         return str(self.re) + str(self.im) + "i"
 
-    def fmandel(self):
-        return self * self + ComplexForDummies.i()
+    def fmandel(self, c):
+        return self * self + c
 
-    def mandel_iterate(self,
-                       tol: float = 1e-4,
-                       itermax: int = 100):
-        z = +self
+    def mandelbrot(self,
+                   itermax: int = 100):
+        z = ComplexForDummies(0, 0)
         stop = False
         convergence = False
         n = 0
-        while not stop and n <= itermax:
-            fz = z.fmandel()
-            diffmod = abs(fz - z)
-            if diffmod <= tol:
-                stop = True
-                convergence = True
-            else:
-                z = fz
-                n += 1
-        return n, convergence
+        while abs(z) < 2.0 and n <= itermax:
+            z = z.fmandel(self)
+            n += 1
+        return n
 
     @classmethod
     def mandel_plot(cls, a0: float = -1, a1: float = 1,
                     b0: float = -1, b1: float = 1,
                     step: float = 0.01):
+
+        if step == 0.0:
+            step = 0.01
+
+        n_step_a = int((a1 - a0) / step)
+        n_step_b = int((b1 - b0) / step)
+        plot_tab = np.ndarray((n_step_a, n_step_b))
+
+        z = ComplexForDummies()
+
+        for ia in range(n_step_a):
+            for ib in range (n_step_b):
+               z.re = a0 + ia * step
+               z.im = b0 + ib * step
+               n, conv = z.mandel_iterate()
 
         pass
